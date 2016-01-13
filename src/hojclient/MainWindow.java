a5ccf5ac3f067332c2b77eeff3fd6518fedbe44a
Rikkokiri <pilvi.rajala@gmail.com>
2016-01-13 13:35:17 +0200

    Hajoaako?

--- src/hojclient/MainWindow.java
+++ src/hojclient/MainWindow.java
@@ -1242,9 +1242,10 @@ public class MainWindow extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
+    	
     	if (args.length < 1){
     		System.exit(0);
-    		}
+    	}
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
