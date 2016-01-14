cbd63601f33dddabd36227658804db0799eab91c
Rikkokiri <pilvi.rajala@gmail.com>
2016-01-14 17:30:36 +0200

    Tehdaskoneiden luonnit lisätty

--- src/hojserver/TehdasImp.java
+++ src/hojserver/TehdasImp.java
@@ -2,13 +2,28 @@ package hojserver;
 
 import java.rmi.*;
 import java.rmi.server.*;
+import hojserver.tehdaskoneet.*;
 
 public class TehdasImp extends UnicastRemoteObject implements Tehdas {
 	
-	
+	//Arrayna vai arraylistin? 
+	private Siilo[] siilot;
+	private Ruuvikuljetin[] ruuvikuljettimet;
+	private Pumppu[] pumput;
+	private Juomakeitin[] juomakeittimet;
+	private Kypsytys[] kypsytyssäiliöt;
 	
 	public TehdasImp() throws RemoteException {
 		super();
+		
+		siilot = new Siilo[4]; //4 siiloa
+		ruuvikuljettimet = new Ruuvikuljetin[3]; //3 kuljetinta
+		pumput = new Pumppu[4]; //4 pumppua
+		juomakeittimet = new Juomakeitin[3];
+		kypsytyssäiliöt = new Kypsytys[10]; //10 kypsytyssäiliötä
+		
+		alustaKoneet();
+		
 	}
 		
 	//Just for the firt test.
@@ -18,6 +33,38 @@ public class TehdasImp extends UnicastRemoteObject implements Tehdas {
 		}
 	}
 	
+	
+	public void alustaKoneet(){
+		/* 
+		 * Olisi varmaan voinut toteuttaa kompaktimmin, mutta oh well...
+		 */
+		
+		//Siilot, 4 kpl
+		for(int i = 0; i < siilot.length; i++){
+			siilot[i] = new Siilo();
+		}
+		
+		//Ruuvikuljettimet, 3 kpl
+		for(int i = 0; i < ruuvikuljettimet.length; i++){
+			ruuvikuljettimet[i] = new Ruuvikuljetin();
+		}
+		
+		//Pumput, 4 kpl
+		//Pitäisikö pullotukseen vievät pumput ja keittimistä kypsytykseen siirtävät pumput eritellä?
+		//Jopa kirjoittaa erilliset luokat? Onko järkeä?
+		
+		
+		//Juomakeittimet, 3 kpl
+		for(int i = 0; i < juomakeittimet.length; i++){
+			juomakeittimet[i] = new Juomakeitin();
+		}
+		
+		//Kypsytyssäiliöt, 10 kpl
+		for(int i = 0; i < kypsytyssäiliöt.length; i++){
+			kypsytyssäiliöt[i] = new Kypsytys();
+		}
+	
+	}
 	
 	
 }
