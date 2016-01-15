c4e16d1be4f1da0811536c9dc725547524a1c633
Rikkokiri <pilvi.rajala@gmail.com>
2016-01-15 14:37:00 +0200

    Methods added to silo. Seems pretty much ready.

--- src/hojserver/tehdaskoneet/Silo.java
+++ src/hojserver/tehdaskoneet/Silo.java
@@ -11,47 +11,78 @@ package hojserver.tehdaskoneet;
 public class Silo {
 
 	private final int capacity = 10000; //kiloa
-	private int filled; //0-10000 kiloa				//Not happy with the name, but I'll go with this
+	private int degreeOfFilling; //0-10000 kiloa				//Not happy with the name, but I'll go with this
 	
+	//Constructor
 	public Silo(){
 		//Oletusarvoisesti siilo on tyhjä
-		filled = 0;
+		degreeOfFilling = 0;
 	}
 	
 	//---- GETTERS AND SETTERS ----
 	
-	public int getKapasiteetti(){
+	public int getCapacity(){
 		return capacity;
 	}
 	
-	public int getTäyttöaste(){
-		return filled;
+	public int getDegreeOfFilling(){
+		return degreeOfFilling;
 	}
 	
 	/**
 	 * 
 	 * @param fill
 	 */
-	public void resetFilled(int fill){ 
+	/*
+	 * TURHA METODI?
+	 */
+	public void resetDegreeOfFilling(int fill){ 
 		if(fill <= capacity){
-			filled = fill;
+			degreeOfFilling = fill;
 		} else{
-			filled = capacity;
+			degreeOfFilling = capacity;
 		}
 	}
 	
 	/**
-	 * 
+	 * Method that adds material to silo.
 	 * @param addition
 	 */
-	public void fillSilo(int addition){
-		if(filled + addition <= capacity){
-			filled += addition;
+	public void addToSilo(int addition){
+		if(degreeOfFilling + addition <= capacity){
+			degreeOfFilling += addition;
 		}
 		else{
 			//TODO
+			
 		}
 	}
 	
+	/**
+	 * Fills the silo to the maximum capacity.
+	 */
+	public void fillSilo(){
+		degreeOfFilling = capacity;
+	}
 	
-}
+	/**
+	 * Method for taking material from the silo. 
+	 * @param take
+	 */
+	public void takeFromSilo(int take){
+		if(take <= degreeOfFilling){
+			degreeOfFilling -= take;
+		} else {
+			System.out.println("Tried to take " + take + " kilos but only " + degreeOfFilling + " kilos in silo. Taking " + degreeOfFilling + " kilos.");
+			degreeOfFilling = 0;
+		}
+	}
+	
+	/**
+	 * Takes everything from the silo.
+	 */
+	public void emptySilo(){
+		degreeOfFilling = 0;
+	}
+	
+} //Processor
