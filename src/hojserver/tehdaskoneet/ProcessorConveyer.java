55130f8366c50c81850aaf6fe40d1854ca069e4f
Rikkokiri <pilvi.rajala@gmail.com>
2016-01-18 15:25:01 +0200

    'Extends Conveyer' added to signature

--- src/hojserver/tehdaskoneet/ProcessorConveyer.java
+++ src/hojserver/tehdaskoneet/ProcessorConveyer.java
@@ -1,11 +1,13 @@
 package hojserver.tehdaskoneet;
 
-public class ProcessorConveyer {
+public class ProcessorConveyer extends Conveyer {
 	
+	private Silo[] silos;
 	private Processor[] processors;
 	
-	public ProcessorConveyer(Processor[] processors){
+	public ProcessorConveyer(Silo[] silos, Processor[] processors){
 		super();
+		this.silos = silos;
 		this.processors = processors;
 	}
 	
