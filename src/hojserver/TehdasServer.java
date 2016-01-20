a20d591fa41c48b958a40b8938e151496cad198c
Rikkokiri <pilvi.rajala@gmail.com>
2016-01-21 00:31:19 +0200

    Siistitty tarpeettomat palaset

--- src/hojserver/TehdasServer.java
+++ src/hojserver/TehdasServer.java
@@ -10,23 +10,15 @@ public class TehdasServer {
 	public static void main(String args[]){
 		
 		try{
-			
-			System.setProperty("java.security.policy", "server.policy");
-			
-			//http://docs.oracle.com/javase/6/docs/technotes/guides/rmi/hello/hello-world.html
-			
+
 			TehdasImp tehdas = new TehdasImp();
 			
+			//Luodaan RMI-rekisteri porttiin 2020
 			Registry registry = LocateRegistry.createRegistry(2020);
-			//Registry registry = LocateRegistry.getRegistry();
-			
-			System.out.println(registry); //for testing
-			
-			//registry.rebind("juomatehdas", tehdas); //TODO Vaihda nimi?
+			System.out.println(registry);
 		
 			Naming.rebind("//127.0.0.1:2020/tehdas", tehdas);
-			
-			
+
 		} catch (Exception e) {
 			System.out.println("Error: " + e);
 		}
