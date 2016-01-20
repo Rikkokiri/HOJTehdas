52b0e192e1796ee26d106bca857d84f3b2a96952
Rikkokiri <pilvi.rajala@gmail.com>
2016-01-20 23:54:25 +0200

    Tehdas-interface siisti

--- src/hojserver/Tehdas.java
+++ src/hojserver/Tehdas.java
@@ -7,21 +7,25 @@ import hojserver.tehdaskoneet.KoneenTila;
 public interface Tehdas extends Remote {
 	
 	//-------- Kirjautuminen--------
-	public void login(String kayttajaNimi) throws RemoteException; // K�ytt�j� kirjautuu
+	
+	public void login(String kayttajaNimi) throws RemoteException; // Käyttäjä kirjautuu
 	
 	public String prosessorinKayttaja(int prosessori) throws RemoteException;
 	
 	//-------- Ruuvikuljettimet --------
+	
 	public void ruuvihihnanKaynnistys() throws RemoteException; //Eli ekan ruuvikuljettimen start nappi
 	
 	public void ruuvihihnanKaynnistysVapautus() throws RemoteException; 
 	
 	//-------- Siilot --------
+	
 	public void siilonVaraus(int siilonNro) throws RemoteException;
 	
 	public void siilonVarausVapautus(int siilonNro) throws RemoteException;
 	
 	//-------- Prosessorit eli keittimet --------
+	
 	public void prosessorinLataus(int kuljettimeNro, int maara) throws RemoteException;
 	
 	public void prosessorinLatausVapautus(int kuljettimeNro) throws RemoteException;
@@ -35,6 +39,7 @@ public interface Tehdas extends Remote {
 	public void prosessorinKaynnistysVapautus(int prosessorinNro) throws RemoteException;
 	
 	//-------- Tankkeja eli kypsytyssäiliöt ja niitä käsittelevät pumput --------
+	
 	public void sailoidenTaytto(int pumpunNro) throws RemoteException; // pumpuilla
 	
 	public void sailoidenTayttoVapautus(int pumpunNro) throws RemoteException; 
@@ -44,11 +49,13 @@ public interface Tehdas extends Remote {
 	public void sailionVarausVapautus(int sailionNro) throws RemoteException;
 	
 	//-------- Pumppaaminen pullotukseen --------
+	
 	public void pullojenTaytto(int pumpunNro) throws RemoteException;
 	
 	public void pullojenTayttoVapautus(int pumpunNro) throws RemoteException;
 	
 	//.-.-.-.-.-. Tietojen kerääminen käyttöliittymän päivittämistä varten .-.-.-.-.-.
+	
 	public int[] siilojenAineMaara() throws RemoteException;
 	
 	public String[] prosessorienTila() throws RemoteException;
