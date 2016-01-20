099d0b88816dd193b9e2adf211e31df64ed17fce
Odixi <vz4ev4r@gmail.com>
2016-01-20 15:32:39 +0200

    Korjaus tilamuutoksissa

--- src/hojserver/tehdaskoneet/ProcessorConveyer.java
+++ src/hojserver/tehdaskoneet/ProcessorConveyer.java
@@ -99,6 +99,7 @@ public class ProcessorConveyer extends Conveyer {
 					}
 					if (silos[siloToBeEmptied].isEmpty())
 						silos[siloToBeEmptied].setTila(KoneenTila.FREE); //?
+						processors[processorToBeFilled].setTila(KoneenTila.FREE); //Tarvitaanko?
 				} // if (jos siirretään)
 				
 				//Tietojen tulostus
